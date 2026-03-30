f3760b5afb06944343bf841bf062dc8e49755337
mena <tsikiniainatsilavina@gmail.com>
2026-03-30 10:19:37 +0300

    First commit

---
+++ src/main/java/org/example/ingredientsrp/IngredientSrpApplication.java
@@ -0,0 +1,13 @@
+package org.example.ingredientsrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientSrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientSrpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ingredientsrp/IngredientSrpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ingredientsrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientSrpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
