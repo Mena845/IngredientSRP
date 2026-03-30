8f9ab90fec50650108f6d900226e68dd2ee196da
mena <tsikiniainatsilavina@gmail.com>
2026-03-30 11:17:48 +0300

    ajout des methodes dans DishRepository et creation de la classe StockRepository

--- src/main/java/org/example/ingredientsrp/repository/DishRepository.java
+++ src/main/java/org/example/ingredientsrp/repository/DishRepository.java
@@ -1,4 +1,46 @@
 package org.example.ingredientsrp.repository;
 
+import org.example.ingredientsrp.dataSource.DataSource;
+import org.example.ingredientsrp.entity.Dish;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
 public class DishRepository {
+
+    private final DataSource dataSource;
+
+    public DishRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Dish findById(int id) throws SQLException {
+
+        String sql = """
+            SELECT d.id, d.name, d.dish_type
+            FROM dish d
+            WHERE d.id = ?
+        """;
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Dish(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        DishTypeEnum.valueOf(rs.getString("dish_type"))
+                );
+            }
+
+            return null;
+        }
+    }
 }
---
+++ src/main/java/org/example/ingredientsrp/repository/StockRepository.java
@@ -0,0 +1,4 @@
+package org.example.ingredientsrp.repository;
+
+public class StockRepository {
+}
