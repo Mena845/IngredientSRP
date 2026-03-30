c30b5411624413b1c86725e1cb76d8d6073d3c62
mena <tsikiniainatsilavina@gmail.com>
2026-03-30 10:54:17 +0300

    ajout des methode dans Ingredient

--- src/main/java/org/example/ingredientsrp/entity/Ingredient.java
+++ src/main/java/org/example/ingredientsrp/entity/Ingredient.java
@@ -1,4 +1,50 @@
 package org.example.ingredientsrp.entity;
 
 public class Ingredient {
+
+    private Integer id;
+    private String name;
+    private String category;
+    private double price;
+
+    public Ingredient() {}
+
+    public Ingredient(Integer id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
