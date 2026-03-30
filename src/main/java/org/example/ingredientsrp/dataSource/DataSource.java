55a612ae805a61b6f39a7b85725d72164d632efe
mena <tsikiniainatsilavina@gmail.com>
2026-03-30 10:51:39 +0300

    ajout des methodes de connection dans DataSource

--- src/main/java/org/example/ingredientsrp/dataSource/DataSource.java
+++ src/main/java/org/example/ingredientsrp/dataSource/DataSource.java
@@ -1,4 +1,18 @@
 package org.example.ingredientsrp.dataSource;
 
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
 public class DataSource {
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/mini_dish_db",
+                "mini_dish_db_manager",
+                "123456"
+        );
+    }
 }
