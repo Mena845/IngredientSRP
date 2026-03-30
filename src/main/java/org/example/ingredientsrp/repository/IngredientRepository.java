f05bbd03f87bb5b37fffa621b4cbb8edbb54c578
mena <tsikiniainatsilavina@gmail.com>
2026-03-30 11:11:30 +0300

    ajout des methode dans IngredientRepository

--- src/main/java/org/example/ingredientsrp/repository/IngredientRepository.java
+++ src/main/java/org/example/ingredientsrp/repository/IngredientRepository.java
@@ -1,4 +1,64 @@
 package org.example.ingredientsrp.repository;
 
+@Repository
 public class IngredientRepository {
+
+    private final DataSource dataSource;
+
+    public IngredientRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Ingredient> findAll(int page, int size) throws SQLException {
+        String sql = """
+            SELECT id, name, price, category
+            FROM ingredient
+            ORDER BY id
+            LIMIT ? OFFSET ?
+        """;
+
+        List<Ingredient> result = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, size);
+            ps.setInt(2, page * size);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                result.add(new Ingredient(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("category"),
+                        rs.getDouble("price")
+                ));
+            }
+        }
+
+        return result;
+    }
+
+    public Ingredient findById(int id) throws SQLException {
+        String sql = "SELECT * FROM ingredient WHERE id = ?";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Ingredient(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("category"),
+                        rs.getDouble("price")
+                );
+            }
+
+            return null;
+        }
+    }
 }
