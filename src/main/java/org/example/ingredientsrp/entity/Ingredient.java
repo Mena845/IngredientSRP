4ae49734dafba557dddedcece978fb32479a5dba
mena <tsikiniainatsilavina@gmail.com>
2026-03-31 10:30:13 +0300

    ajout d'un constructeur public dans Ingredient

--- src/main/java/org/example/ingredientsrp/entity/Ingredient.java
+++ src/main/java/org/example/ingredientsrp/entity/Ingredient.java
@@ -1,21 +1,31 @@
 package org.example.ingredientsrp.entity;
+
 import lombok.Getter;
 import lombok.Setter;
-
 
 public class Ingredient {
 
     private Integer id;
+
     @Setter
     private String name;
+
     private String category;
+
     @Getter
     private double price;
+
     private Dish dish;
+
     @Getter
     @Setter
     private Double requiredQuantity;
 
+    // ✅ Constructeur par défaut obligatoire pour Jackson
+    public Ingredient() {
+    }
+
+    // Tes autres constructeurs existants
     public Ingredient(int id, String name, CategoryEnum category, double price) {
         this.id = id;
         this.name = name;
@@ -39,30 +49,17 @@ public class Ingredient {
         this.dish = dish;
     }
 
+    // Getters & Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
+    public String getName() { return name; }
 
-    public Integer getId() {
-        return id;
-    }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    public void setPrice(double price) { this.price = price; }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
+    public Dish getDish() { return dish; }
+    public void setDish(Dish dish) { this.dish = dish; }
 }
